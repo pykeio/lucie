5d8d00c530d59471d612a825f48da98ead27a1cb
Carson M. <carson@pyke.io>
2026-05-18 15:11:02 -0500

    [u] Fix `on_hover` callbacks
    Upstreams f18b288fdf06d2b0d66bb4832bb0d9897dc827ba

--- crates/gpui/src/elements/div.rs
+++ crates/gpui/src/elements/div.rs
@@ -1689,8 +1689,8 @@ impl Interactivity {
 				if phase == DispatchPhase::Capture && hovered != was_hovered {
 					if let Some(hover_state) = &hover_state {
 						hover_state.borrow_mut().element = hovered;
+						cx.notify(current_view);
 					}
-					cx.notify(current_view);
 				}
 			});
 		}
@@ -1863,7 +1863,7 @@ impl Interactivity {
 
 			if let Some(hover_listener) = self.hover_listener.take() {
 				let hitbox = hitbox.clone();
-				let was_hovered = element_state.hover_state.get_or_insert_with(Default::default).clone();
+				let was_hovered = element_state.hover_listener_state.get_or_insert_with(Default::default).clone();
 				let has_mouse_down = element_state.pending_mouse_down.get_or_insert_with(Default::default).clone();
 
 				window.on_mouse_event(move |_: &MouseMoveEvent, phase, window, cx| {
@@ -1873,8 +1873,8 @@ impl Interactivity {
 					let is_hovered = has_mouse_down.borrow().is_none() && !cx.has_active_drag() && hitbox.is_hovered(window);
 					let mut was_hovered = was_hovered.borrow_mut();
 
-					if is_hovered != was_hovered.element {
-						was_hovered.element = is_hovered;
+					if is_hovered != *was_hovered {
+						*was_hovered = is_hovered;
 						drop(was_hovered);
 
 						hover_listener(&is_hovered, window, cx);
@@ -2149,6 +2149,7 @@ pub struct InteractiveElementState {
 	pub(crate) focus_handle: Option<FocusHandle>,
 	pub(crate) clicked_state: Option<Rc<RefCell<ElementClickedState>>>,
 	pub(crate) hover_state: Option<Rc<RefCell<ElementHoverState>>>,
+	pub(crate) hover_listener_state: Option<Rc<RefCell<bool>>>,
 	pub(crate) pending_mouse_down: Option<Rc<RefCell<Option<MouseDownEvent>>>>,
 	pub(crate) scroll_offset: Option<Rc<RefCell<Point<Pixels>>>>,
 	pub(crate) active_tooltip: Option<Rc<RefCell<Option<ActiveTooltip>>>>,
